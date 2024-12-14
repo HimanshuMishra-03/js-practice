ac1082914b89d564e09204b75a0d114632a4744d
HimanshuMishra-03 <138067227+HimanshuMishra-03@users.noreply.github.com>
2024-12-14 20:45:51 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+console.log("02">1);//true, it converts "02" to int 02
+console.log("2">1);//true, it converts "2" to 2
+
+console.log(null>0);//false
+console.log(null == 0);//false
+console.log(null<=0);//true
+console.log(null>=0);//true
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined>=0);//false
+console.log(undefined<=0);//false
+
+
+
+
+
+
+
+
