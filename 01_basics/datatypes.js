d42a867710508b4e0ce88b91c14f8d3561c4a1ca
HimanshuMishra-03 <138067227+HimanshuMishra-03@users.noreply.github.com>
2024-12-14 20:11:50 +0000

    dataypes and var

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treats all JS code as newer version
+// using alert(3+3) will show error because we are using node js and not browser
+//number
+//bigint
+//string
+//boolean
+//null--> standalone value
+//undefine
+//symbol 
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+console.log(typeof 10);
+console.log(typeof "Himanshu");
+
---
+++ 01_basics/var.js
@@ -0,0 +1,9 @@
+const accountId=69;
+let accountName="Himanshu";
+var account="Himanshu__";
+console.log(accountId);
+console.log(accountName);
+console.log(account);
+/*Prefer not to use var keyword because it gives scope problem,
+which was later solved by let keyword*/ 
+
