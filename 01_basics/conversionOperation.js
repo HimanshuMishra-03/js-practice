7b197efd448baaa0b8fc2eb2842d1643abaa7d5a
HimanshuMishra-03 <138067227+HimanshuMishra-03@users.noreply.github.com>
2024-12-14 20:37:44 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,35 @@
+let score=undefined;
+console.log(typeof score);
+let scoreInNumber=Number(score);
+console.log(scoreInNumber);
+console.log(typeof scoreInNumber);
+let something=null;
+console.log(something);
+something=undefined;
+console.log(something);
+
+
+//"33"==>33
+//"33abc"==>NaN
+//true=1 false=0
+let s="Hitesh";
+let boolOfs=Boolean(s);
+console.log(boolOfs);
+s="";
+boolOfs=Boolean(s);
+console.log(boolOfs);
+
+let n=69;
+let Stringn=String(n);
+console.log(Stringn);
+console.log(typeof Stringn);
+
+let val=3;
+let negVal=-val;
+console.log(negVal);
+
+console.log(+true);//it will show 1 instead of true
+
+
+
+
